f7716288843d64d38db78a5bc0d65f6e7d74f47a
Complexor <jozbornn@gmail.com>
2021-02-04 19:12:09 -0800

    Room registration / Variable rearrangement
    Enables use of a for loop instead of if/elif/elif...; also put variables at the top of the module

--- microgames/thriftech/game.py
+++ microgames/thriftech/game.py
@@ -1,14 +1,32 @@
 from random import randint
 from enum import Enum
 
+cmd = []
+
 
 class Room(Enum):
 
-    AT_MACHINE = 0
-    AT_WORKBENCH = 1
-    AT_COMPUTER = 2
-    QUIT_GAME = 3
+    AT_MACHINE = "machine"
+    AT_WORKBENCH = "workbench"
+    AT_COMPUTER = "computer"
+    QUIT_GAME = "quit"
 
+
+room = Room.AT_MACHINE
+
+state_functions = {}
+
+desc = {
+    "machine": "You are at the recycling machine.",
+    "workbench": "You are at the workbench.",
+    "computer": "You are at the defunct computer."
+}
+
+menu = {
+    "machine": "PROCESS TRASH\tWORKBENCH\tCOMPUTER\tINVENTORY\tQUIT",
+    "workbench": "CRAFT\tMACHINE\tCOMPUTER\tINVENTORY\tQUIT",
+    "computer": "CRAFT\tLOOK\tFIX\tMACHINE\tWORKBENCH\tINVENTORY\tQUIT"
+}
 
 computer = {
     "psu": False,
@@ -20,36 +38,6 @@ computer = {
     "ram": False
 }
 
-
-def computer_is_fixed():
-    return all(computer.values())
-
-
-def reset_computer():
-    for each in computer:
-        computer[each] = False
-
-
-material_recipes = {
-    "circuit board": (("glass", 1), ("plastic", 1), ("copper wire", 1)),
-    "cable": (("copper wire", 2), ("plastic", 1)),
-    "microchip": (("scrap metal", 1), ("plastic", 1), ("glass", 1)),
-    "capacitor": (("scrap metal", 1), ("copper wire", 1), ("glass", 1)),
-    "casing": (("scrap metal", 2), ("plastic", 1), ("glass", 1))
-}
-
-component_recipes = {
-    "psu": (("casing", 1), ("cable", 2), ("capacitor", 3)),
-    "motherboard": (("circuit board", 2), ("cable", 1), ("microchip", 1)),
-    "processor": (("microchip", 2), ("cable", 2), ("capacitor", 1)),
-    "graphics card": (("casing", 1), ("microchip", 2), ("circuit board", 1)),
-    "hard drive": (("casing", 1), ("circuit board", 1), ("cable", 2)),
-    "solid state drive": (("casing", 1), ("circuit board", 1), ("microchip", 1)),
-    "ram": (("microchip", 1), ("cable", 1), ("circuit board", 1))
-}
-
-
-cmd = []
 inventory = {
     "glass": 0,
     "scrap metal": 0,
@@ -73,14 +61,38 @@ inventory = {
     "computer": 0
 }
 
+material_recipes = {
+    "circuit board": (("glass", 1), ("plastic", 1), ("copper wire", 1)),
+    "cable": (("copper wire", 2), ("plastic", 1)),
+    "microchip": (("scrap metal", 1), ("plastic", 1), ("glass", 1)),
+    "capacitor": (("scrap metal", 1), ("copper wire", 1), ("glass", 1)),
+    "casing": (("scrap metal", 2), ("plastic", 1), ("glass", 1))
+}
+
+component_recipes = {
+    "psu": (("casing", 1), ("cable", 2), ("capacitor", 3)),
+    "motherboard": (("circuit board", 2), ("cable", 1), ("microchip", 1)),
+    "processor": (("microchip", 2), ("cable", 2), ("capacitor", 1)),
+    "graphics card": (("casing", 1), ("microchip", 2), ("circuit board", 1)),
+    "hard drive": (("casing", 1), ("circuit board", 1), ("cable", 2)),
+    "solid state drive": (("casing", 1), ("circuit board", 1), ("microchip", 1)),
+    "ram": (("microchip", 1), ("cable", 1), ("circuit board", 1))
+}
+
+
+def computer_is_fixed():
+    return all(computer.values())
+
+
+def reset_computer():
+    for each in computer:
+        computer[each] = False
+
 
 def reset_inventory():
     for each in inventory:
         if each != "computer":
             inventory[each] = 0
-
-
-room = Room.AT_MACHINE
 
 
 def view_inventory():
@@ -193,9 +205,18 @@ def universal_check():
     return False
 
 
+def register_room(room):
+
+    def register_func(f):
+        state_functions[room.value] = f
+
+    return register_func
+
+
 # Process trash for ingredients
+@register_room(Room.AT_MACHINE)
 def at_machine():
-    if check_cmd("process", "trash"):
+    if check_cmd("process"):
         a = randint(0, 2)
         inventory["glass"] += a
         if a > 0:
@@ -222,6 +243,7 @@ def at_machine():
 
 
 # Craft ingredients into materials
+@register_room(Room.AT_WORKBENCH)
 def at_workbench():
     if check_cmd("craft"):
         if len(cmd) == 1:
@@ -242,8 +264,9 @@ def at_workbench():
 
 # Build materials into components
 # Place the components into the computer
+@register_room(Room.AT_COMPUTER)
 def at_computer():
-    if check_cmd(["look", "view", "check"], "computer") or check_cmd("computer"):
+    if check_cmd(["look", "view", "check", "computer"]):
         for each in computer:
             if not computer[each]:
                 print("The %s is missing." % each)
@@ -264,7 +287,7 @@ def at_computer():
                     craft(each, component_recipes[each])
                     return True
 
-    elif check_cmd("fix", "computer"):
+    elif check_cmd("fix"):
         at_least_one = False
         for each in computer:
             if not computer[each] and inventory[each] >= 1:
@@ -275,10 +298,6 @@ def at_computer():
         if not at_least_one:
             print("You have no components to put in the computer.")
         return True
-
-    elif check_cmd(["exit", "quit"]):
-        global room
-        room = Room.QUIT_GAME
 
     return False
 
@@ -295,38 +314,24 @@ def run_thriftech(data=None):
 
     while not end:
         valid_command = False
-        if room == Room.AT_MACHINE:
-            print("You are at the recycling machine.")
-            print("PROCESS TRASH\tWORKBENCH\tCOMPUTER\tINVENTORY\tQUIT")
+
+        if room == Room.QUIT_GAME:
+            room = Room.AT_MACHINE
+            end = True
+            valid_command = True
+        else:
+            for each_room in Room:
+                if room == each_room:
+                    print(desc[room.value], menu[room.value], sep="\n")
+                    break
+
             get_cmd()
-            if not universal_check():
-                valid_command = at_machine()
-            else:
-                valid_command = True
-        elif room == Room.AT_WORKBENCH:
-            print("You are at the workbench.")
-            print("CRAFT\tMACHINE\tCOMPUTER\tINVENTORY\tQUIT")
-            get_cmd()
-            if not universal_check():
-                valid_command = at_workbench()
-            else:
-                valid_command = True
-        elif room == Room.AT_COMPUTER:
-            print("You are at the defunct computer.")
-            print("CRAFT\tLOOK COMPUTER\tFIX COMPUTER\tMACHINE\tWORKBENCH\tINVENTORY\tQUIT")
-            get_cmd()
-            if not universal_check():
-                valid_command = at_computer()
-            else:
-                valid_command = True
+
+            valid_command = True if universal_check() else state_functions[room.value]()
+
             if computer_is_fixed():
                 end = True
                 print("You have completely fixed the computer.")
-        elif room == Room.QUIT_GAME:
-            room = Room.AT_MACHINE
-            print("GOODBYE!")
-            end = True
-            valid_command = True
 
         if not valid_command:
             print("I don't understand \"%s\"" % " ".join(cmd))
